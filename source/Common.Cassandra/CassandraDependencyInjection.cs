10d82f60b92da980d9f0222876da8b11c357dd51
tranvuongduy2003 <t-vuongduy@pasonatech.vn>
2024-10-30 10:34:26 +0700

    Add dependency injection

---
+++ source/Common.Cassandra/CassandraDependencyInjection.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Cassandra;
+
+public static class CassandraDependencyInjection
+{
+    public static IServiceCollection AddCassandra<TDbContextImplementation, TDbContext>(
+        this IServiceCollection services,
+        IConfiguration cassandraConfiguration)
+        where TDbContext : class, ICassandraDbContext
+        where TDbContextImplementation : CassandraDbContext, TDbContext
+    {
+        services.AddSingleton<TDbContextImplementation>();
+        services.AddSingleton<ICassandraDbContext>(provider => provider.GetRequiredService<TDbContextImplementation>());
+
+        var cassandraOptions = (CassandraOptions<TDbContext>)cassandraConfiguration.Get<CassandraOptions>();
+        if (cassandraOptions == null)
+            throw new ArgumentNullException("Cassandra is not configured.");
+
+        services
+            .AddSingleton<CassandraOptions<TDbContext>>(cassandraOptions);
+
+        return services;
+    }
+}
